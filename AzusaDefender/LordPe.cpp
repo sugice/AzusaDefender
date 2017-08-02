cdf68ca885ca5870a517d32b9e6787e0e79a003d
徒然 <pengjunting2@qq.com>
2017-08-02 16:59:35 +0800

    基本信息解析完毕

--- AzusaDefender/LordPe.cpp
+++ AzusaDefender/LordPe.cpp
@@ -10,3 +10,89 @@ CLordPe::CLordPe()
 CLordPe::~CLordPe()
 {
 }
+
+void CLordPe::GetBasicInfo(CString& filePath)
+{
+	// 1. 打开文件,将文件读取到内存.
+	// CreateFile,ReadFile.
+	HANDLE hFile = INVALID_HANDLE_VALUE;
+	hFile = CreateFile(filePath,GENERIC_READ,FILE_SHARE_READ,NULL,
+						OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,NULL);
+	if (hFile == INVALID_HANDLE_VALUE)
+	{
+		return;
+	}
+	DWORD dwFileSize = 0;
+	dwFileSize = GetFileSize(hFile, NULL);
+
+	// 2. 申请内存空间
+	BYTE* pBuf = new BYTE[dwFileSize];
+
+	// 3. 将文件内容读取到内存中
+	DWORD dwRead = 0;
+	ReadFile(hFile,pBuf,dwFileSize,&dwRead,NULL);
+
+	// 将缓冲区当成DOS头结构体来解析
+	IMAGE_DOS_HEADER* pDosHdr;// DOS头
+	pDosHdr = (IMAGE_DOS_HEADER*)pBuf;
+	IMAGE_NT_HEADERS* pNtHdr;
+
+	// nt头,包含这文件头和扩展头
+	pNtHdr = (IMAGE_NT_HEADERS*)(pDosHdr->e_lfanew + (DWORD)pBuf);
+
+	// 判断是否是一个有效的pe文件
+	if (pDosHdr->e_magic != IMAGE_DOS_SIGNATURE|| pNtHdr->Signature != IMAGE_NT_SIGNATURE)
+	{
+		AfxMessageBox(_T("不是有效的PE文件"));
+		return;
+	}
+
+	IMAGE_FILE_HEADER* pFileHdr; // 文件头
+	IMAGE_OPTIONAL_HEADER* pOptHdr;// 扩展头
+	pFileHdr = &(pNtHdr->FileHeader);
+	pOptHdr = &(pNtHdr->OptionalHeader);
+	
+	//文件头内的信息
+	m_basicInfo.NumberOfSections = pFileHdr->NumberOfSections;
+	m_basicInfo.SizeOfOptionalHeader = pFileHdr->SizeOfOptionalHeader;
+	m_basicInfo.Characteristics = pFileHdr->Characteristics;
+	//扩展头的信息
+	m_basicInfo.Magic = pOptHdr->Magic;
+	m_basicInfo.SizeOfCode = pOptHdr->SizeOfCode;
+	m_basicInfo.AddressOfEntryPoint = pOptHdr->AddressOfEntryPoint;
+	m_basicInfo.BaseOfCode = pOptHdr->BaseOfCode;
+	m_basicInfo.BaseOfData = pOptHdr->BaseOfData;
+	m_basicInfo.ImageBase = pOptHdr->ImageBase;
+	m_basicInfo.SectionAlignment = pOptHdr->SectionAlignment;
+	m_basicInfo.FileAlignment = pOptHdr->FileAlignment;
+	m_basicInfo.SizeOfImage = pOptHdr->SizeOfImage;
+	m_basicInfo.SizeOfHeaders = pOptHdr->SizeOfHeaders;
+	m_basicInfo.DllCharacteristics = pOptHdr->DllCharacteristics;
+	m_basicInfo.NumberOfRvaAndSizes = pOptHdr->NumberOfRvaAndSizes;
+
+	PIMAGE_DATA_DIRECTORY pDataDirectory = pOptHdr->DataDirectory;
+	DWORD i = 0;
+	while (i < pOptHdr->NumberOfRvaAndSizes)
+	{
+		DataTableOfContents dataTableObj = { 0 };
+		dataTableObj.VirtualAddress = pDataDirectory[i].VirtualAddress;
+		dataTableObj.Size = pDataDirectory[i].Size;
+		m_vecDataTable.push_back(dataTableObj);
+		++i;
+	}
+
+	IMAGE_SECTION_HEADER* pScnHdr = NULL;
+	pScnHdr = IMAGE_FIRST_SECTION(pNtHdr);
+
+	for (int i = 0; i < pFileHdr->NumberOfSections; ++i)
+	{
+		SectionTable sectionTable = { 0 };
+		sectionTable.Name = pScnHdr[i].Name;
+		sectionTable.VirtualSize = pScnHdr[i].Misc.VirtualSize;
+		sectionTable.SizeOfRawData = pScnHdr[i].SizeOfRawData;
+		sectionTable.PointerToRawData = pScnHdr[i].PointerToRawData;
+		sectionTable.PointerToVirData = pScnHdr[i].VirtualAddress + pOptHdr->ImageBase;
+		sectionTable.Characteristics = pScnHdr[i].Characteristics;
+		m_vecSectionTable.push_back(sectionTable);
+	}
+}
--- AzusaDefender/LordPe.h
+++ AzusaDefender/LordPe.h
@@ -1,8 +1,56 @@
 #pragma once
+#include <vector>
+using std::vector;
+
+
+//基本信息
+typedef struct _BASICINFO
+{
+	WORD NumberOfSections;//区段数量
+	WORD SizeOfOptionalHeader;//扩展头大小
+	WORD Characteristics;//文件属性
+
+	WORD Magic;//文件类型标志
+	DWORD SizeOfCode;//代码段总大小
+	DWORD AddressOfEntryPoint;//入口点
+	DWORD BaseOfCode;//代码基址
+	DWORD BaseOfData;//数据基址
+	DWORD ImageBase;//默认加载基址
+	DWORD SectionAlignment;//块对齐
+	DWORD FileAlignment;//文件块对齐
+	DWORD SizeOfImage;//镜像大小
+	DWORD SizeOfHeaders;//头部总大小
+	WORD DllCharacteristics;//DLL特征
+	DWORD NumberOfRvaAndSizes;//数据目录个数
+}BASICINFO,*PBASICINFO;
+
+//数据目录表
+typedef struct _DataTableOfContents
+{
+	DWORD VirtualAddress;//相对虚拟地址
+	DWORD Size;//大小
+}DataTableOfContents,*PDataTableOfContents;
+
+//区段表
+typedef struct _SectionTable
+{
+	CString Name;//区段名
+	DWORD VirtualSize;//实际大小
+	DWORD SizeOfRawData;//文件中大小
+	DWORD PointerToRawData;//文件中位置
+	DWORD PointerToVirData;//内存中位置
+	DWORD Characteristics;//区段属性
+}SectionTable, *PSectionTable;
+
 class CLordPe
 {
 public:
 	CLordPe();
 	~CLordPe();
+public:
+	void GetBasicInfo(CString& filePath);
+	BASICINFO m_basicInfo;
+	vector<DataTableOfContents> m_vecDataTable;
+	vector<SectionTable> m_vecSectionTable;
 };
 
